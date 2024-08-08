77e2f8ff6c79288be3f12b3221536e370fabf52d
ProggerX <x@proggers.ru>
2024-08-08 22:23:16 +0300

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+	};
+	outputs = { nixpkgs, ... }:
+	let systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+	in {
+		packages = builtins.listToAttrs (builtins.map (system: { name = system; value =
+		let pkgs = import nixpkgs { inherit system; };
+		in { default = pkgs.stdenv.mkDerivation {
+			name = "bal";
+			version = "v2.0.0";
+			src = ./.;
+			nativeBuildInputs = with pkgs; [ gnumake gcc ];
+			buildPhase = "make build";
+			installPhase = "mkdir -p $out/bin && cp out $out/bin/bal";
+		};};}) systems );
+	};
+}
